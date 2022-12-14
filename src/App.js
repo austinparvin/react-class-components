fcd132a7851acc119e130219baba760f0d9d76b6
Austin Parvin <austin.parvin@gmail.com>
2022-12-14 08:13:19 +0100

    add render component, utiliaze shouldComponentUpdate via state and props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import { useState } from "react";
 import Constructor from "./components/Constructor";
+import Render from "./components/Render";
 
 function App() {
+    const [counter, setCounter] = useState(0);
     return (
         <div className="App">
             <header className="App-header">
+                <Render number={counter} />
+                <button onClick={() => setCounter(counter + 1)}>
+                    App add 1
+                </button>
+                <hr />
                 <Constructor name={"constructor()"} />
             </header>
         </div>
---
+++ src/components/Render.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { initialCounterState } from "../constants";
+
+class Render extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.onRenderClick = this.onRenderClick.bind(this);
+        this.state = initialCounterState;
+    }
+    onRenderClick() {
+        this.setState({ counter: this.state.counter + 1 });
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextState.counter > 3) {
+            return false;
+        }
+        return nextProps.number < 3 ? true : false;
+    }
+    render() {
+        return (
+            <div>
+                <h2>Rendered Component Counter</h2>
+                <h2>shouldComponentUpdate</h2>
+                <h3>Render Counter: {this.state.counter}</h3>
+                <button onClick={this.onRenderClick}>Render Add 1</button>
+                <h3>App Counter: {this.props.number}</h3>
+            </div>
+        );
+    }
+}
+
+export default Render;
